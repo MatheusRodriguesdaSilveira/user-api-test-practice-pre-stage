04ef4554a7f1b4b2690b13b8df4c9764857e1af4
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-04 18:53:44 -0300

    feat: update user service

---
+++ src/services/update-user-service.ts
@@ -0,0 +1,46 @@
+import { db } from "../prisma";
+
+interface UserRequest {
+  user_id: string;
+  name: string;
+}
+
+class UpdateUserService {
+  async execute({ user_id, name }: UserRequest) {
+    if (!user_id) {
+      throw new Error("ID do usuário é obrigatório");
+    }
+
+    const userExists = await db.user.findFirst({
+      where: {
+        id: user_id,
+      },
+    });
+
+    if (!userExists) {
+      throw new Error("Usuário nao encontrado");
+    }
+
+    if (!name || name.trim() === "") {
+      throw new Error("Nome é obrigatório");
+    }
+
+    const user = await db.user.update({
+      where: {
+        id: user_id,
+      },
+      data: {
+        name,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
+  }
+}
+
+export { UpdateUserService };
