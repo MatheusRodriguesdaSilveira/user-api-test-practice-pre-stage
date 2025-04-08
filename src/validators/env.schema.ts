7eee368cbe14f203eafb3c7ab62ab3907548a14d
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-08 17:10:32 -0300

    feat(validator): improves structure

---
+++ src/validators/env.schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import "dotenv/config";
+
+const envSchema = z.object({
+  // Server
+  PORT: z.coerce.number().default(3333),
+
+  // Database
+  DATABASE_URL: z.string(),
+
+  // URLs
+  API_URL: z.string().url(),
+  WEB_URL: z.string().url(),
+});
+
+export const env = envSchema.parse(process.env);
