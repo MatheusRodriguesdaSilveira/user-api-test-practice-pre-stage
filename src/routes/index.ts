313e5bcfffc9ff0dfd59a4b87675e39db3141866
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-08 17:05:45 -0300

    refactor(routes): improves structure and export

---
+++ src/routes/index.ts
@@ -0,0 +1,4 @@
+export { createUserRoute } from "./user/create.route";
+export { getUsersRoute } from "./user/get-all.route";
+export { updateUserRoute } from "./user/update.route";
+export { deleteUserRoute } from "./user/delete.route";
