6623ed43bb9fad87159cc344b547c62a448a55ef
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-08 17:06:50 -0300

    refactor(service): improves structure and export

---
+++ src/services/index.ts
@@ -0,0 +1,4 @@
+export { CreateUserService } from "./user/create.service";
+export { GetUsersService } from "./user/get-all.service";
+export { UpdateUserService } from "./user/update.service";
+export { DeleteUserService } from "./user/delete.service";
