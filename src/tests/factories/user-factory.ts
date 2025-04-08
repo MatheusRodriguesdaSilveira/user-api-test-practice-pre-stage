bb63c0662d6efac7ab42eb687a30b54aa2419a0b
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-08 17:09:03 -0300

    feat(factories): add id

--- src/tests/factories/user-factory.ts
+++ src/tests/factories/user-factory.ts
@@ -1,8 +1,13 @@
 import { db } from "../../prisma";
 
-export async function createUser(data?: { name?: string; email?: string }) {
+export async function createUser(data?: {
+  id?: string;
+  name?: string;
+  email?: string;
+}) {
   return db.user.create({
     data: {
+      id: data?.id || `user-${Date.now()}`,
       name: data?.name || "Usuário de Teste",
       email: data?.email || `user${Date.now()}@test.com`,
     },
