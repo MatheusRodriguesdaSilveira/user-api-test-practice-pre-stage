bc1bccb5a5a31e0a52a961f115d5cb335e015458
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-08 14:28:35 -0300

    Revert "feat: change struct server"
    This reverts commit 69c0251428aa26fedafed8afceebe31d2b5f5e1c.

--- src/server.ts
+++ src/server.ts
@@ -1,60 +1,19 @@
+import express from "express";
 import dotenv from "dotenv";
-import { env } from "./env";
-import fastifyCors from "@fastify/cors";
-import { fastifySwagger } from "@fastify/swagger";
-import { fastifySwaggerUi } from "@fastify/swagger-ui";
-import { fastify } from "fastify";
-import {
-  jsonSchemaTransform,
-  serializerCompiler,
-  validatorCompiler,
-} from "fastify-type-provider-zod";
-import { createUserRoute } from "./routes/create-user";
-import { getUsersRoute } from "./routes/get-users";
-import { updateUserRoute } from "./routes/update-user";
+import { router } from "./routes/routes";
 
 dotenv.config();
 
-const app = fastify();
-app.register(fastifyCors);
-
-app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: "User API Test Practice (Pré-Estágio)",
-      description:
-        "Mini aplicação Node.js com foco em boas práticas de testes, validação de usuários e integração com banco de dados.",
-      version: "1.0.0",
-    },
-  },
-  transform: jsonSchemaTransform,
-});
-
-app.register(fastifySwaggerUi, {
-  routePrefix: "/docs",
-});
-
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
-
-// Routes
-app.register(createUserRoute);
-app.register(getUsersRoute);
-app.register(updateUserRoute);
-
-// Server
-const port = env.PORT || 3333;
+const app = express();
+app.use(express.json());
+app.use("/api", router);
 
 if (process.env.NODE_ENV !== "test") {
-  app
-    .listen({
-      host: "0.0.0.0",
-      port,
-    })
-    .then(() => {
-      console.log(`Server: http://localhost:${port} is running ⚡⚡⚡`);
-      console.log(`Swagger: http://localhost:${port}/docs`);
-    });
+  app.listen(process.env.PORT || 3333, () =>
+    console.log(
+      `Server: http://localhost:${process.env.PORT || 3333} is running ⚡⚡⚡`
+    )
+  );
 }
 
 export default app;
