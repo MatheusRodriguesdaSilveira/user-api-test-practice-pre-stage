c03e21ac51b9bffcb509ec7dc83d10ff145a6247
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-08 17:03:54 -0300

    refactor(controller): improve server structure and typings

---
+++ src/controllers/index.ts
@@ -0,0 +1,4 @@
+export { CreateUserController } from "./user/create.controller";
+export { GetUsersController } from "./user/get-all.controller";
+export { UpdateUserController } from "./user/update.controller";
+export { DeleteUserController } from "./user/delete.controller";
