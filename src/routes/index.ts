d9c02400cb5e6e71d81eaadf61200ac7373c6f26
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-09 19:52:34 -0300

    refactor(routes): new improves

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
-export { createUserRoute } from "./user/create.route";
+export { createUserRoute } from "./auth/create-user.route";
+export { authUserRoute } from "./auth/auth-user.route";
 export { getUsersRoute } from "./user/get-all.route";
+export { getUserByIdRoute } from "./user/get-by-id.route";
 export { updateUserRoute } from "./user/update.route";
 export { deleteUserRoute } from "./user/delete.route";
