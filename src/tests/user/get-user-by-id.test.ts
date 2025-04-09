8563d76151981a36e726bc894efd857103bb2edc
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-09 20:06:26 -0300

    feat(tests): add tests for the GetUserById service to validate user retrieval scenarios

---
+++ src/tests/user/get-user-by-id.test.ts
@@ -0,0 +1,33 @@
+import { db } from "../../prisma";
+import { GetUserByIdService } from "../../services";
+import { createUser } from "../factories/user-factory";
+
+describe("GetUserById", () => {
+  const service = new GetUserByIdService();
+
+  beforeEach(async () => {
+    await db.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await db.$disconnect();
+  });
+
+  // Teste para verificar se o serviço retorna um usuário com sucesso
+  it("deve retorna um usuário com sucesso", async () => {
+    const createdUser = await createUser();
+
+    const user = await service.execute(createdUser.id);
+
+    expect(user).toHaveProperty("id");
+    expect(user.name).toBe(createdUser.name);
+    expect(user.email).toBe(createdUser.email);
+  });
+
+  // Teste para verificar se o serviço retorna um erro ao tentar buscar um usuário sem ID
+  it("deve retornar um erro ao tentar buscar um usuário sem ID", async () => {
+    await expect(service.execute("")).rejects.toThrow(
+      "ID do usuário é obrigatório"
+    );
+  });
+});
