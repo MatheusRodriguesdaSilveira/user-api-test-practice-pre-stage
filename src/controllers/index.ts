09842a73516677531dbd63c28d65e23b9294996b
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-09 19:52:10 -0300

    refactor(controller): new improves

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,6 @@
-export { CreateUserController } from "./user/create.controller";
+export { CreateUserController } from "./auth/create-user.controller";
+export { AuthUserController } from "./auth/auth-user.controller";
 export { GetUsersController } from "./user/get-all.controller";
+export { GetUserByIdController } from "./user/get-by-id.controller";
 export { UpdateUserController } from "./user/update.controller";
 export { DeleteUserController } from "./user/delete.controller";
