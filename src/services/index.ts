8c559ee30ba6100866c590930183bf4d091602ad
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-09 19:53:05 -0300

    refactor(service): new improves

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
-export { CreateUserService } from "./user/create.service";
+export { CreateUserService } from "./auth/create-user.service";
+export { AuthUserService } from "./auth/auth-user.service";
 export { GetUsersService } from "./user/get-all.service";
+export { GetUserByIdService } from "./user/get-by-id.service";
 export { UpdateUserService } from "./user/update.service";
 export { DeleteUserService } from "./user/delete.service";
