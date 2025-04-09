f376530ce5c8f18ffa8bc08571f37d8b8df46df0
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-09 20:06:07 -0300

    feat(tests): add tests for DeleteUserService to validate user deletion scenarios

--- src/tests/user/delete-user.test.ts
+++ src/tests/user/delete-user.test.ts
@@ -1,1 +1,44 @@
-// proxima feature: user delete test
+import { db } from "../../prisma";
+import { DeleteUserService } from "../../services";
+import { createUser } from "../factories/user-factory";
+
+describe("DeleteUserService", () => {
+  const service = new DeleteUserService();
+
+  beforeEach(async () => {
+    await db.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await db.$disconnect();
+  });
+
+  // Teste para verificar se o serviço deleta um usuário com sucesso
+  it("deve deletar um usuário com sucesso", async () => {
+    const createdUser = await createUser();
+
+    const user = await db.user.delete({
+      where: {
+        id: createdUser.id,
+      },
+    });
+
+    expect(user).toHaveProperty("id");
+    expect(user.name).toBe(createdUser.name);
+    expect(user.email).toBe(createdUser.email);
+  });
+
+  // Teste para verificar se o serviço falha ao deletar um usuário sem ID
+  it("deve falhar se o ID do usuário estiver vazio", async () => {
+    await expect(service.execute("")).rejects.toThrow(
+      "ID do usuário é obrigatório"
+    );
+  });
+
+  // Teste para verificar se o serviço falha ao deletar um usuário que não existe
+  it("deve falhar se o usuário não existir", async () => {
+    await expect(service.execute("1")).rejects.toThrow(
+      "Usuário não encontrado"
+    );
+  });
+});
