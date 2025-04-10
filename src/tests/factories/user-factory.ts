d00d666f33f30f78cef4f3fa0b3484fb081acad2
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-10 16:46:43 -0300

    feat(tests): refactor user creation tests and update user factory to hash passwords

--- src/tests/factories/user-factory.ts
+++ src/tests/factories/user-factory.ts
@@ -1,15 +1,20 @@
+import { hash } from "bcryptjs";
 import { db } from "../../prisma";
 
 export async function createUser(data?: {
   id?: string;
   name?: string;
   email?: string;
+  password?: string;
 }) {
+  const hashedPassword = await hash(data?.password || "password123", 8);
+
   return db.user.create({
     data: {
       id: data?.id || `user-${Date.now()}`,
       name: data?.name || "Usuário de Teste",
       email: data?.email || `user${Date.now()}@test.com`,
+      password: hashedPassword,
     },
   });
 }
