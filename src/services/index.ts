998fd5c1ee209be8f705104f7f4f8d25d7fff6fd
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-11 16:54:26 -0300

    feat(services): add category services to the index export

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,13 @@
+// Auth
 export { CreateUserService } from "./auth/create-user.service";
 export { AuthUserService } from "./auth/auth-user.service";
+
+// User
 export { GetUsersService } from "./user/get-all.service";
 export { GetUserByIdService } from "./user/get-by-id.service";
 export { UpdateUserService } from "./user/update.service";
 export { DeleteUserService } from "./user/delete.service";
+
+// Category
+export { CreateCategoryService } from "./category/create.service";
+export { GetCategoriesService } from "./category/get-all.service";
