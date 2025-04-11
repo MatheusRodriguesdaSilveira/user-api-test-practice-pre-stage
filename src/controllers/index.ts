ee60db7ab1a3cba57fab9cfd971e5b70bab3a1a0
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-11 16:53:31 -0300

    feat(controllers): add category controllers to the index export

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,13 @@
+// Auth
 export { CreateUserController } from "./auth/create-user.controller";
 export { AuthUserController } from "./auth/auth-user.controller";
+
+// User
 export { GetUsersController } from "./user/get-all.controller";
 export { GetUserByIdController } from "./user/get-by-id.controller";
 export { UpdateUserController } from "./user/update.controller";
 export { DeleteUserController } from "./user/delete.controller";
+
+// Category
+export { CreateCategoryController } from "./category/create.controller";
+export { GetCategoriesController } from "./category/get-all.controller";
