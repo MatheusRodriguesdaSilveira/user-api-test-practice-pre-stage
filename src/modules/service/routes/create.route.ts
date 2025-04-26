32734dfd1f545545ffbc0902e02482edec6a0bc5
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-04-25 21:55:10 -0300

    feat(route): add file upload middleware and enhance service creation schema

--- src/modules/service/routes/create.route.ts
+++ src/modules/service/routes/create.route.ts
@@ -1,26 +1,37 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { CreateServiceController } from "../controllers";
+import { upload } from "../../../shared/middlewares/upload";
+import multer from "fastify-multer";
+
+const uploadMiddleware = multer(upload.upload("uploads"));
 
 export const createServiceRoute: FastifyPluginAsyncZod = async (app) => {
   app.post(
     "/service",
     {
+      preHandler: uploadMiddleware.single("file"),
       schema: {
         summary: "Create Service",
         description: "Create a new service",
         operationId: "createService",
         tags: ["service"],
-        body: z.object({
-          name: z.string().min(1).max(100),
-        }),
         response: {
           201: z.object({
             id: z.string(),
             name: z.string(),
+            description: z.string(),
+            image: z.string(),
+            category_id: z.string(),
           }),
           400: z.object({
-            error: z.string(),
+            message: z.string(),
+          }),
+          404: z.object({
+            message: z.string(),
+          }),
+          500: z.object({
+            message: z.string(),
           }),
         },
       },
