f15caecf1551ab01849beeac570a1bef20bb92de
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-05-09 22:31:18 -0300

    feat(validators): add validation for Cloudinary environment variables

--- src/validators/env.schema.ts
+++ src/validators/env.schema.ts
@@ -14,6 +14,11 @@ const envSchema = z.object({
   // URLs
   API_URL: z.string().url(),
   WEB_URL: z.string().url(),
+
+  // Cloudinary
+  CLOUDINARY_NAME: z.string(),
+  CLOUDINARY_KEY: z.string(),
+  CLOUDINARY_SECRET: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
