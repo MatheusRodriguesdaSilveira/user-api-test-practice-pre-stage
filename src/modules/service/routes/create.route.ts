85c061f3ea4c2dffad18694a84fc33a21b638677
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-05-09 22:33:54 -0300

    feat(route): remove multer upload middleware from create service route

--- src/modules/service/routes/create.route.ts
+++ src/modules/service/routes/create.route.ts
@@ -1,16 +1,11 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { CreateServiceController } from "../controllers";
-import { upload } from "../../../shared/middlewares/upload";
-import multer from "fastify-multer";
-
-const uploadMiddleware = multer(upload.upload("uploads"));
 
 export const createServiceRoute: FastifyPluginAsyncZod = async (app) => {
   app.post(
     "/service",
     {
-      preHandler: uploadMiddleware.single("file"),
       schema: {
         summary: "Create Service",
         description: "Create a new service",
