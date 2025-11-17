ac9500037a6e6c700df77ef0a6895a3d64741352
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-11-17 17:53:31 -0300

    feat: add frontend in the project

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+import { env } from "./validators/env.schema";
+import { resolve } from "path";
+
 import {
   authUserRoute,
   createUserRoute,
@@ -5,25 +9,29 @@ import {
   getUserByIdRoute,
   getUsersRoute,
   updateUserRoute,
-} from "./modules/users/routes";
-import { createCategoryRoute, getCategoriesRoute } from "./modules/category";
+} from "./mvc/modules/users/routes";
+
+import {
+  createCategoryRoute,
+  getCategoriesRoute,
+} from "./mvc/modules/category";
+
 import {
   createServiceRoute,
   deleteServiceRoute,
+  getServiceIdRoute,
   getServicesRoute,
-} from "./modules/service";
+} from "./mvc/modules/service";
 
-import dotenv from "dotenv";
-import { env } from "./validators/env.schema";
-
-import { fastify } from "fastify";
+import fastifyFormbody from "@fastify/formbody";
+import fastifyCookie from "@fastify/cookie";
 import fastifyCors from "@fastify/cors";
-import { fastifySwagger } from "@fastify/swagger";
-import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import fastifyView from "@fastify/view";
 import fastifyStatic from "@fastify/static";
 import multipart from "@fastify/multipart";
-
-import { resolve } from "path";
+import { fastify } from "fastify";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 
 import {
   jsonSchemaTransform,
@@ -31,9 +39,21 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 
-dotenv.config();
+import { serviceWebRoutes } from "./mvc/modules/web/routes/serviceWeb.route";
+import { homeWebRoutes } from "./mvc/modules/web/routes/homeWeb.route";
+import { authWebRoutes } from "./mvc/modules/web/routes/authWeb.route";
+import { categoryWebRoutes } from "./mvc/modules/web/routes/categoryWeb.route";
+import { appointmentWebRoutes } from "./mvc/modules/web/routes/appointmentWebRoutes";
+
+dotenv.config({
+  path: resolve(process.cwd(), ".env"),
+});
 
 const app = fastify();
+
+app.register(fastifyCookie);
+
+app.register(fastifyFormbody);
 
 app.register(multipart, {
   limits: {
@@ -46,8 +66,13 @@ app.register(fastifyCors, {
 });
 
 app.register(fastifyStatic, {
-  root: resolve(process.cwd(), "uploads"),
-  prefix: "/uploads/",
+  root: resolve(__dirname, "..", "public"),
+  prefix: "/public/",
+});
+
+app.register(fastifyView, {
+  engine: { ejs: require("ejs") },
+  root: resolve(process.cwd(), "src", "mvc", "modules", "web", "view"),
 });
 
 app.register(fastifySwagger, {
@@ -88,6 +113,14 @@ app.register(getCategoriesRoute);
 app.register(createServiceRoute);
 app.register(getServicesRoute);
 app.register(deleteServiceRoute);
+app.register(getServiceIdRoute);
+
+// Web Routes
+app.register(homeWebRoutes);
+app.register(serviceWebRoutes);
+app.register(authWebRoutes);
+app.register(categoryWebRoutes);
+app.register(appointmentWebRoutes);
 
 // Server
 const port = env.PORT || 3333;
@@ -101,6 +134,7 @@ if (process.env.NODE_ENV !== "test") {
     .then(() => {
       console.log(`Server: http://localhost:${port} is running ⚡⚡⚡`);
       console.log(`Swagger: http://localhost:${port}/docs`);
+      console.log(`Web: http://localhost:${port}/admin/service/list`);
     });
 }
 
