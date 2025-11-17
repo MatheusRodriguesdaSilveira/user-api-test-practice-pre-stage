abc6ecf6f260c70538facbd6cee9efa712a923f1
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-11-17 17:36:49 -0300

    feat: add tailwind in the project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/mvc/modules/web/view/**/*.ejs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
